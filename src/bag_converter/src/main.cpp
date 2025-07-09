243bd241766cad5356411cd85a58d37a49606590
syx-37 <1419150535@qq.com>
2025-07-09 14:11:28 +0800

    20250708-分离一下头文件和cpp文件（测试通过版

--- src/bag_converter/src/main.cpp
+++ src/bag_converter/src/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "bag_to_cloud_info_node");
     
     // 创建两个设备的转换器实例
+    BagToCloudInfoConverter converter_0("0");
     BagToCloudInfoConverter converter_1("1");
-    BagToCloudInfoConverter converter_2("2");
     
     ROS_INFO("Bag to cloud info converter node started");
     
